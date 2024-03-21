467a7288ca046abeb21ca7bd7785d6b624507974
Minou <am.mihoubi@gmail.com>
2024-03-21 23:43:59 +0100

    02-CustomUser and Profile models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#Custom User
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=50, unique=True)
+    mobile = models.CharField(max_length=10, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+#Profile
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+                                    "self", 
+                                    related_name="followed_by",
+                                    symmetrical=False,
+                                    blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+
