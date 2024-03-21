da311c12f2b805535105436909f624fbd3c50c49
Minou <am.mihoubi@gmail.com>
2024-03-22 00:19:23 +0100

    03-Add signal to connect User with Profile automatically and make it follow himself

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 #Custom User
 class CustomUser(AbstractUser):
@@ -23,5 +24,16 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
+    
+#Make Profile when new User is added
+def make_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        #Make the new profile follow himself
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+
+post_save.connect(make_profile, sender=CustomUser)
 
 
